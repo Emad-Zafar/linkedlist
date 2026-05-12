a6711559cd7e9d669dc55ea092e27e5df4d1e834
EmadZafar <emad.zafar@insightcorp.health>
2026-05-12 22:56:06 +0500

    linked list with append, appendAt and delete operations

---
+++ linkedlist.js
@@ -0,0 +1,238 @@
+
+// step -1 create class for node object creation
+class Node{
+    constructor(value,next){
+        this.value=value;
+        this.next=null;
+    }
+}
+
+
+// step -2: Actual linkedList class declearation - starts from head and goes on head -> node -> node -> ...
+class List {
+    constructor(head){
+        this.head = head;
+        this.size = 0;
+    }
+
+    // just node creation
+    create (value){
+        return new Node(value);
+    }
+
+    append (value){
+
+        //create node
+        const node = list.create(value);
+
+        //check if list is empty add at at first
+        if (!this.head){
+            node.next = this.head;
+            this.head = node;
+            this.size++;
+            return;
+        }
+        //traverse the list and append in the last 
+        else {
+            let current = this.head
+            while (current.next){
+                current = current.next;
+
+            }
+            current.next = node;
+            this.size++;
+            return;
+                
+        }
+    }
+
+
+    appendAt (value, index) {
+
+        //create node
+        const node = list.create(value);
+
+        if (index > this.size) {
+            return console.log(`\n "${value}" at "${index}" index can not be added \n==> Index does not exist...\n`);
+
+        }
+        //check if list is empty add at at first
+        else if (!this.head || index === 0) {
+            node.next = this.head;
+            this.head = node;
+            this.size++;
+            return;
+        }
+
+        //traverse the list and append on the index 
+        else {
+            let current = this.head;
+            let previous;
+            let count = 0;
+
+            while (count < index){
+                previous = current;
+                current = current.next;
+                count++;
+
+
+            }
+            previous.next = node;
+            node.next = current;
+            this.size++;
+            return;
+                
+        }
+        
+    }
+
+    delete (value){
+
+        let current = this.head;
+        let previous;
+
+        if (!this.head) {
+            return console.log(`list is empty`);
+        }
+        else if (this.head.value == value ) {
+            this.head = current.next;
+            console.log(`first node deleted new head is ${this.head.value}`)
+            this.size--;
+            return;
+        }
+        else {
+            while (!!current && current.value != value){
+                
+                previous = current;
+                current = current.next;
+            }
+            if (!current) {
+                return console.log(`value ${value} does not exist in the list`);
+            }
+            previous.next = current.next;
+            this.size--;
+            return;
+
+        }
+
+    }
+
+
+    deleteAt (index){
+        if (this.size == 0){
+            return console.log (`List is empty...`)
+        }
+        else if (index == 0 && this.size == 1){
+            this.head = null;
+            this.size--;
+            return console.log (`Now the list is empty`);
+        }
+        else {
+            
+        }
+    }
+
+
+}
+
+//linkedlist object creation 
+const list = new List(null);
+console.log(list);
+
+
+
+
+//operations
+list.append(5);
+list.append(4);
+list.append(3);
+list.append(2);
+list.append(1);
+list.appendAt(100,2);
+list.delete(200);
+console.dir(list,{depth:null})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Node{
+//     constructor(value,next){
+//         this.value=value;
+//         this.next=null;
+//     }
+// }
+
+// class List {
+//     constructor(head){
+//         this.head = head
+//     }
+
+//     create (value){
+//         return new Node(value);
+//     }
+
+//     append (value){
+//         const node = this.create(value);
+
+//         if (!this.head){
+//             this.head = node;
+//         }
+//         else {
+//             let current = this.head
+//             while (current.next){
+//                 current = current.next;
+
+//             }
+//             current.next = node;
+                
+//         }
+//     }
+// }
+// const list = new List(null);
+// console.log(list)
+// list.append(5);
+// list.append(4);
+// list.append(3);
+// list.append(2);
+// list.append(1);
+// console.dir(list,{depth:null})
+
+
+
+// console.log(list)
+//appending first node
+
+
+
+
+// console.log(node1.value)
+// console.log(node1.next)
